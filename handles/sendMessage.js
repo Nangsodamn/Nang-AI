6bb6f6c06c734eb0c2474c1024ccd16a11481b44
Nangsodamn <nangmwa02@gmail.com>
2026-03-20 02:16:43 +0800

    Update sendMessage.js

--- handles/sendMessage.js
+++ handles/sendMessage.js
@@ -1,1 +1,17 @@
+const axios = require("axios");
 
+async function sendMessage(senderId, message, pageAccessToken) {
+  try {
+    await axios.post(
+      `https://graph.facebook.com/v18.0/me/messages?access_token=${pageAccessToken}`,
+      {
+        recipient: { id: senderId },
+        message: message
+      }
+    );
+  } catch (err) {
+    console.log("Error:", err.response?.data || err.message);
+  }
+}
+
+module.exports = { sendMessage };
